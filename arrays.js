454ac07da409d7310039116b63a238c2f54f764b
Kenneth Bilicke <kbilicke7891@gmail.com>
2018-03-07 01:34:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,23 @@ function addElementToEndOfArray(array,element) {
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.push(element)
 }
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift()
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length-1)
+}
